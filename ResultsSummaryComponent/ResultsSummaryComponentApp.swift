f1e4be902b40fecc31bab8d160c0d7959c2b3e32
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-04-01 15:37:26 +0900

    Initial Commit

---
+++ ResultsSummaryComponent/ResultsSummaryComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResultsSummaryComponentApp.swift
+//  ResultsSummaryComponent
+//
+//  Created by Jinwook Kim on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct ResultsSummaryComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
